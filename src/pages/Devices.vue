dcf04e43a0339030308a0b6da9e85f2e641551dc
José Henrique Merlo de Sousa <jhmerlo@outlook.com>
2023-04-13 16:56:07 -0300

    devices page

--- src/pages/Devices.vue
+++ src/pages/Devices.vue
@@ -15,17 +15,63 @@
       </div>
     </div>
     <div class="row q-col-gutter-sm q-px-sm">
-        <div class="col-md-2 col-sm-4 col-xs-12" v-for="i in 14" :key="i">
-          <q-card>
+        <div class="col-md-3 col-sm-4 col-xs-12" v-for="computer, id in computers" :key="id">
+          <q-card class="fit">
+            <div class="absolute-top-right q-pa-sm text-caption text-grey-9 text-weight-bold">
+              #{{ computer.id }}
+            </div>
+            <div class="absolute-top-left q-pa-sm text-caption text-grey-9 text-weight-bold">
+              <q-chip class="q-ma-none text-caption" size="sm" color="yellow-9" text-color="white" icon="hardware">
+                Testes de Hardware
+              </q-chip>
+            </div>
             <q-card-section class="q-pb-none">
-              <div class="text-primary text-center">
+              <div class="text-primary text-center q-mt-lg">
+                <span class="text-weight-bold absolute-center q-pt-lg q-mt-xs">
+                  {{ computer.current_step }}
+                </span>
                 <q-icon size="70px" name="computer" />
               </div>
-              <div class="text-caption text-grey-8 text-center">
-                Pc Gamer Intel Core i7 3770-3.90 GHz, 16GB RAM, NVIDIA 4GB, SSD 512GB
+            </q-card-section>
+              <q-card-section class="q-pa-sm">
+              <div class="ellipsis text-caption text-center">
+                {{computer.description}}
+                <q-tooltip>
+                  {{ computer.description }}
+                </q-tooltip>
               </div>
             </q-card-section>
-            <q-card-actions align="right">
+            <q-card-section class="q-pt-none" style="min-height: 160px">
+              <div>
+                <div v-if="computer.patrimony" class="text-caption text-grey-9">
+                  <b>Patrimônio UFES:</b> {{ computer.patrimony }}
+                </div>
+                <div class="q-mt-xs text-caption text-grey-9">
+                  <b>Fabricante:</b> {{ computer.manufacturer }}
+                </div>
+                <div class="q-mt-xs text-caption text-grey-9">
+                  <b>Responsável:</b> {{ computer.responsible.name }}
+                </div>
+                <div class="q-mt-md text-caption text-grey-9">
+                  <q-icon 
+                    :name="computer.sanitized ? 'check_circle' : 'warning'" 
+                    :color="computer.sanitized ? 'positive' : 'warning'" 
+                    size="xs"  
+                  />
+                  {{ computer.sanitized ? 'Higienizado' : 'Não Higienizado' }}
+                </div>
+                <div class="q-mt-xs text-caption text-grey-9">
+                  <q-icon 
+                    :name="computer.functional ? 'check_circle' : 'cancel'" 
+                    :color="computer.functional ? 'positive' : 'negative'" 
+                    size="xs"  
+                  />
+                  {{ computer.functional ? 'Funcional' : 'Não Funcional' }}
+                </div>
+              </div>
+            </q-card-section>
+            <div style="height: 40px"/>
+            <q-card-actions align="right" class="q-pt-xl absolute-bottom">
               <q-btn
                 color="grey-8"
                 icon="visibility"
@@ -37,7 +83,7 @@
                 </q-tooltip>
               </q-btn>
               <q-btn
-                @click="generateQrCode"
+                @click="generateQrCode(computer.id)"
                 color="grey-8"
                 icon="qr_code"
                 round
@@ -51,6 +97,14 @@
           </q-card>
         </div>
     </div>
+    <div class="q-pa-lg flex flex-center">
+      <q-pagination
+        @input=getComputers
+        v-model="page"
+        :max="lastPage"
+      />
+    </div>
+    
     <q-dialog v-model="dialogTest">
       <q-card class="text-center">
         <div class="text-h6 text-primary">
@@ -66,24 +120,42 @@
 
 <script>
 import QRious from 'qrious'
+import { maintenanceSteps, maintenanceColors } from 'src/utils/constants'
+
 export default {
   name: 'Devices',
   data: () => ({
-    dialogTest: false
+    dialogTest: false,
+    page: 1,
+    lastPage: 1,
+    computers: [],
+    maintenanceSteps: { ...maintenanceSteps },
+    maintenanceColors: { ...maintenanceColors }
   }),
   methods: {
-    generateQrCode () {
+    generateQrCode (id) {
       this.dialogTest = true
       setTimeout(() => {
         new QRious({
           level: 'H',
           padding: 5,
-          size: 300,
+          size: 200,
           element: document.getElementById('qr-code-canvas'),
-          value: 'jhmerlo@outlook.com'
+          value: String(id)
         })
       }, 200)
+    },
+    async getComputers () {
+      const { data } = await this.$axios.get('computers', {params: { page: this.page }});
+
+      this.computers = data.data
+      this.page = data.current_page
+      this.lastPage = data.last_page
+      console.log(data)
     }
+  },
+  created () {
+    this.getComputers()
   }
 }
 </script>
---
+++ src/utils/constants.js
@@ -0,0 +1,17 @@
+export const maintenanceSteps = {
+  1: 'Triagem',
+  2: 'Testes de Hardware',
+  3: 'Manutenção',
+  4: 'Rede e Periféricos',
+  5: 'Testes de Usuário',
+  6: 'Liberação'
+}
+
+export const maintenanceColors = {
+  1: 'grey',
+  2: 'yellow',
+  3: 'purple',
+  4: 'orange',
+  5: 'blue',
+  6: 'green'
+}
